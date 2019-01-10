63354501c34e38741dcf5c8361daf8d64a9c94eb
AJ Clemente <areljann.c@gmail.com>
2019-01-10 18:26:54 -0500

    Added AboutMe section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar   from './_components/Navbar';
 import Home     from './_sections/Home';
 import Projects from './_sections/Projects';
+import AboutMe  from './_sections/AboutMe';
 
 import './_stylesheets/custom.css';
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
           <Navbar   />
           <Home     />
           <Projects />
+          <AboutMe  />
         </div>
       </Router>
     );
