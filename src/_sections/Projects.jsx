ec6e0c985c39b64ac8f54e67c9a66c2aed68d3c3
AJ Clemente <areljann.c@gmail.com>
2019-01-10 14:59:22 -0500

    Testing out the extent of Array.map()

--- src/_sections/Projects.jsx
+++ src/_sections/Projects.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import { Element } from 'react-scroll';
 
-const projects = [];
+import PoseCard from './../_pose-components/PoseCard';
+
+const projects = [
+  {
+    thumbnail: "#",
+    title: "Placeholder",
+    description: "Just a bunch of random text for the description",
+    tags: ["HTML", "CSS", "JavaScript"],
+    linkInfo: "#",
+    linkLive: "#"
+  }
+];
 
 const Projects = () => (
   <Element name="projects" id="c-section-projects">
@@ -9,6 +20,23 @@ const Projects = () => (
       <div className="container">
         <div className="content has-text-centered">
           <h1>Projects</h1>
+        </div>
+        <div className="columns is-centered">
+          <div className="column">
+            {
+              projects.map((project) => (
+                <div>
+                  <p>Project name: {project.title}</p>
+                  <p>Tags: {
+                      project.tags.map((tag) => (
+                        <span>{tag}</span>
+                        ))
+                      }
+                  </p>
+                </div>
+              ))
+            }
+          </div>
         </div>
       </div>
     </div>
