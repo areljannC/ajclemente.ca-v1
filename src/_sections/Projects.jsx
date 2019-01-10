368134faf935f9497b108345bbe60562ed129171
AJ Clemente <areljann.c@gmail.com>
2019-01-10 13:03:38 -0500

    Created Projects section

---
+++ src/_sections/Projects.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Element } from 'react-scroll';
+
+const projects = [];
+
+const Projects = () => (
+  <Element name="projects" id="c-section-projects">
+    <div className="section">
+      <div className="container">
+        <div className="content has-text-centered">
+          <h1>Projects</h1>
+        </div>
+      </div>
+    </div>
+  </Element>
+);
+
+export default Projects;
