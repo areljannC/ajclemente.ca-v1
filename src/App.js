ce7226a79e29bb40ed0ed0f9bb1e200edf0a810f
AJ Clemente <areljann.c@gmail.com>
2019-01-10 13:22:01 -0500

    Added Projects section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import Navbar from './_components/Navbar';
-import Home   from './_sections/Home';
+import Navbar   from './_components/Navbar';
+import Home     from './_sections/Home';
+import Projects from './_sections/Projects';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Navbar />
-          <Home   />
+          <Navbar   />
+          <Home     />
+          <Projects />
         </div>
       </Router>
     );
