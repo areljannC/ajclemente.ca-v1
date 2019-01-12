4c52047e88094cf62f374646c3f576923273630f
AJ Clemente <areljann.c@gmail.com>
2019-01-12 16:14:15 -0500

    Created a Footer component

---
+++ src/_components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Element } from 'react-scroll';
+
+const Footer = () => (
+  <Element name="footer" id="c-footer">
+    <div className="footer">
+      <div className="container">
+        <div className="content has-text-centered">
+          <div className="level">
+            <div className="level-item">
+              <p className="is-size-6 c-text-color-white">© Arel Jann Clemente 2019</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Element>
+);
+
+export default Footer;
