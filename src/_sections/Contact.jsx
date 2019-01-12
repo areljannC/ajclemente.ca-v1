fb33ee1ce5ad2816ba0534e6cc1b7ecf6acacccb
AJ Clemente <areljann.c@gmail.com>
2019-01-12 16:16:22 -0500

    Added a form with validation using Formik and Yup

--- src/_sections/Contact.jsx
+++ src/_sections/Contact.jsx
@@ -29,7 +29,17 @@ const Contact = () => (
         </div>
         <div className="columns is-centered">
           <div className="column is-two-fifths">
-            {/* Insert a message */}
+            <div className="content">
+              <div className="section has-text-centered">
+                <p className="is-size-5 c-text-color-black">
+                  <span className="is-size-4 c-text-underline">
+                    Thanks for visiting my website!
+                  </span> <br/><br/>
+                  If you have any questions or you want to get in touch,
+                  feel free to give me a message anytime.
+                </p>
+              </div>
+            </div>
           </div>
           <div className="column">
             <Formik
@@ -39,19 +49,55 @@ const Contact = () => (
                 message: "",
               }}
               validationSchema={ContactSchema}
-              onSubmit={values => {
+              onSubmit={(values, actions) => {
                 alert(JSON.stringify(values));
+                actions.resetForm(values = {
+                  name:    "",
+                  email:   "",
+                  message: "",
+                });
               }}
             >
-              {({ errors, touched }) => (
+              {({ errors, touched, isSubmitting }) => (
                 <Form>
-                  <Field component="input" name="name" />
-                  { errors.name && touched.name ? ( <div>{errors.name}</div> ) : null }
-                  <Field component="input" name="email" />
-                  { errors.email && touched.email ? ( <div>{errors.email}</div> ) : null }
-                  <Field component="textarea" name="message" />
-                  { errors.message && touched.message ? ( <div>{errors.message}</div> ) : null }
-                  <button type="submit">Submit</button>
+                  <div className="field" data-aos="fade-right">
+                    <label className="label c-text-color-black">Name</label>
+                    <p className="control has-icons-left">
+                      <Field component="input" name="name"
+                        className={ errors.name && touched.name ? ("input c-input-error") : ("input") }
+                      />
+                      <span className={ errors.name && touched.name ? ("icon is-small is-left c-text-color-red") : ("icon is-small is-left c-text-color-black") }>
+                        <i className="fas fa-user"></i>
+                      </span>
+                    </p>
+                    { errors.name && touched.name ? ( <p className="help c-text-color-red">{errors.name}</p> ) : null }
+                  </div>
+                  <div className="field" data-aos="fade-left">
+                    <label className="label c-text-color-black">Email</label>
+                    <p className="control has-icons-left">
+                      <Field component="input" name="email"
+                        className={ errors.email && touched.email ? ("input c-input-error") : ("input") }
+                      />
+                      <span className={ errors.email && touched.email ? ("icon is-small is-left c-text-color-red") : ("icon is-small is-left c-text-color-black") }>
+                        <i className="fas fa-envelope"></i>
+                      </span>
+                    </p>
+                    { errors.email && touched.email ? ( <p className="help c-text-color-red">{errors.email}</p> ) : null }
+                  </div>
+                  <div className="field" data-aos="fade-right">
+                    <label className="label c-text-color-black">Message</label>
+                    <div className="control">
+                      <Field component="textarea" name="message" placeholder="Leave a message and I'll be in touch!"
+                        className={ errors.message && touched.message ? ("textarea c-input-error") : ("textarea") }
+                      />
+                    </div>
+                    { errors.message && touched.email ? ( <p className="help c-text-color-red">{errors.message}</p> ) : null }
+                  </div>
+                  <div className="control" data-aos="fade-left" data-aos-offset={-10}>
+                    <button type="submit" disabled={isSubmitting} className="button is-fullwidth c-button c-text-color-white">
+                      Send
+                    </button>
+                  </div>
                 </Form>
               )}
             </Formik>
