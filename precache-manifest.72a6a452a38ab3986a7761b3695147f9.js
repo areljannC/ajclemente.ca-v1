969c9db9c303c4c24dfa3d67252fff7cf42ff197
AJ Clemente <areljann.c@gmail.com>
2019-01-13 14:58:06 -0500

    Updates

---
+++ precache-manifest.72a6a452a38ab3986a7761b3695147f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "54b163d2d2fa0339007e",
+    "url": "/static/css/main.ff19b735.chunk.css"
+  },
+  {
+    "revision": "54b163d2d2fa0339007e",
+    "url": "/static/js/main.54b163d2.chunk.js"
+  },
+  {
+    "revision": "5949f4c2780a5d2b521b",
+    "url": "/static/js/1.5949f4c2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a43727fa74b0fc7bc249c0fb7cf7ef14",
+    "url": "/static/media/aj-logo-red.a43727fa.png"
+  },
+  {
+    "revision": "63350934ac288b3e8c5404682a47db9e",
+    "url": "/static/media/aj-logo-black.63350934.png"
+  },
+  {
+    "revision": "c5df12fd434e9207b9d8e488f5d9a0e8",
+    "url": "/static/media/illustrator.c5df12fd.png"
+  },
+  {
+    "revision": "72a2bc0c37a99ddfe1d73afcc4904fd7",
+    "url": "/index.html"
+  }
+];
